7e646b901072f09db93f0e341be80b8e55a97029
Kamden Wright <600022173@fjuhsd.org>
2021-02-23 22:34:53 +0000

    Created a getNumVowels and getLength method

--- Word.java
+++ Word.java
@@ -8,14 +8,58 @@ public class Word
 {
    private static String vowels = "AEIOUaeiou";   	//why is this here?
    																//why is it static?
-   
-   //add an instance variable
 
+   //add an instance variable
+private String s;
 	//create a constructor
+	public Word()
+  {
+    s = "";
+  }
+
+  public Word(String str)
+  {
+    s = str;
+  }
+
+//Getter method
+public String getWord()
+{
+  return s;
+}
+
+//Setter method
+public void setWord(String n)
+{
+  s = n;
+}
+
+//getNumVowels
+public int getNumVowels(){
+
+    int x = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if ( vowels.indexOf(s.charAt(i)) >= 0 ){
+        x++;
+      }
+       
+    }
+  return x;
+}
+
+
+//getLength
 	
-	//create other methods
-	//look at the sample runner code
-	//on the lab handout
+public int getLength(){
+
+  return s.length();
+
+}
 
 	//create a toString method
+public String toString()
+{
+  return ""+ s;
+}
+
 }
